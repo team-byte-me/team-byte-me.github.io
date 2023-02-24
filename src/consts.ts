1c9bf26e6f5c36173badf4c29b734d9ed59de596
Jake Hubbard <49132883+jdhubb99@users.noreply.github.com>
2023-02-24 04:59:23 +0000

    Add milestone 1 post

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Team Byte Me';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Byte Me 🐶'
+export const SITE_DESCRIPTION = 'Site to host Team Byte Me&apos;s term project for CSCI 4800 Human Computer Interaction.';
